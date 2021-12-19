e7d013694718aa7cfad5cbb23c6794b851059683
Mishuni <mmms543.keti@gmail.com>
2021-12-19 23:08:27 +0900

    Init Package

--- setup.py
+++ setup.py
@@ -4,14 +4,26 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+username = "keti_test"
 setup(
-    name='test', 
+    name='test_'+username, 
     version='0.1',
-    description='test package',
     author='test1234',
     author_email='test1234@email.re.kr',
+    description='test package',
     long_description=long_description,
     python_requires='>=3.6',
     long_description_content_type="text/markdown",
     packages= find_packages(exclude = ['docs', 'tests*','__pycache__/']),
+    url="https://github.com/Mishuni/Pip_Package_Practice.git",
+    python_requires='>=3.6',
+    install_requires=[
+        'Flask'
+    ],
+    entry_points={
+        'console_scripts': [
+            'test-main=test_package.__main__:main',
+            'tpm=test_package.__main__:main',
+        ],
+    },
 )
---
+++ test_package/__main__.py
@@ -0,0 +1,7 @@
+from modules import add
+
+def main():
+    print(add(2,3))
+
+if __name__=="__main__":
+    main()
